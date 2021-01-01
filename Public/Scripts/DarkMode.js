3a2a6a868b39d1066418904c30f530f959414651
Miguel Henrique <miguelhp373@gmail.com>
2020-12-31 21:20:40 -0300

    31/12 - Adicionado Modal de Adiçao da Açoes

--- Public/Scripts/DarkMode.js
+++ Public/Scripts/DarkMode.js
@@ -7,12 +7,15 @@ function active(){
    localStorage.setItem('active','enable')
    document.querySelector('.nav-bar-top').style.backgroundColor = '#2A2F32'
    document.querySelector('.content-cards').style.backgroundColor = '#131C21'
+   document.querySelector('.exit-button-icon').style.backgroundColor ='#202e36'
    document.getElementsByTagName("footer").style.backgroundColor = '#131C21'
  }
  if(!ative.checked){
    localStorage.setItem('active','disabled')
    document.querySelector('.nav-bar-top').style.backgroundColor = '#27ae60'
    document.querySelector('.content-cards').style.backgroundColor = '#EFEFEF'
+   document.querySelector('.exit-button-icon').style.backgroundColor ='#20914f'
+
  }
 
 }
---
+++ Public/Scripts/NewActions.js
@@ -0,0 +1,89 @@
+function addMoney(){
+    document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+  
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+            </div>
+            <div class="modal-footer">
+                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
+                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
+                        style="background-color:#a3130d;">Se Inscrever</button></a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+  </div>`
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+function NewDespesa(){
+    document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+  
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+            </div>
+            <div class="modal-footer">
+                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
+                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
+                        style="background-color:#a3130d;">Se Inscrever</button></a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+  </div>`
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+function MyPlanejament(){
+    document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+  
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+            </div>
+            <div class="modal-footer">
+                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
+                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
+                        style="background-color:#a3130d;">Se Inscrever</button></a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+  </div>`
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+
+
+
+
+
