61ea477f7feb2e31a8de263d9f2e79a9d9ab7f3f
Miguel Henrique <miguelhp373@gmail.com>
2021-01-10 20:14:13 -0300

    New App interface
    # Add Dark Mode
    # New Side Bar
    # New Effects

--- Public/Scripts/DarkMode.js
+++ Public/Scripts/DarkMode.js
@@ -1,23 +1,16 @@
 //darkmode
-function active(){
+const ActiveDarkMode = document.querySelector('#DarkMode');
 
- var ative = document.querySelector('#switch-2')
+ActiveDarkMode.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  document.querySelector('.header').classList.toggle('dark');
+  document.querySelector('#sidebar-wrapper').classList.toggle('dark');
+  document.querySelector('#hamburguer').classList.toggle('dark');
+  document.querySelector('#nav-icon').classList.toggle('dark');
+  
+});
 
- if(ative.checked){
-   localStorage.setItem('active','enable')
-   document.querySelector('.nav-bar-top').style.backgroundColor = '#2A2F32'
-   document.querySelector('.content-cards').style.backgroundColor = '#131C21'
-   document.querySelector('.exit-button-icon').style.backgroundColor ='#202e36'
-   document.getElementsByTagName("footer").style.backgroundColor = '#131C21'
- }
- if(!ative.checked){
-   localStorage.setItem('active','disabled')
-   document.querySelector('.nav-bar-top').style.backgroundColor = '#27ae60'
-   document.querySelector('.content-cards').style.backgroundColor = '#EFEFEF'
-   document.querySelector('.exit-button-icon').style.backgroundColor ='#20914f'
 
- }
 
-}
 
 
--- Public/Scripts/NewActions.js
+++ Public/Scripts/NewActions.js
@@ -2,20 +2,26 @@ function addMoney(){
     document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-  
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        
+            <div class="modal-header" style='background-color: rgb(39, 174, 96);' >
+            <h1 style='font-size:20px;'>Nova Receita</h1>               
             </div>
             <div class="modal-body">
-                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+                <label for='new-input'>Valor da Receita:</label>
+                <input class ='new-input' type='number' placeholder='R$'/>
+                <br>
+                <input class = 'new-input' type='text' placeholder='Descrição'/>
+                <br>
+                <select id='add-money-categorias' class='select-categorias'>
+                <option>Categoria</option>
+                <option value='0'>Salário</option>
+                <option value='1'>Troco</option>
+                <option value='2'>Econômias</option>
+                </select>
             </div>
             <div class="modal-footer">
-                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
-                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
-                        style="background-color:#a3130d;">Se Inscrever</button></a>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+               <button type="button" class="btn btn-success">Confirmar</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
             </div>
         </div>
     </div>
@@ -30,20 +36,27 @@ function NewDespesa(){
     document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-  
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        
+            <div class="modal-header" style='background-color:#d63031;'>
+            <h1 style='font-size:20px;'>Nova Despesa</h1>
+                                
             </div>
             <div class="modal-body">
-                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+                <label for='new-input'>Valor da Despesa:</label>
+                <input class ='new-input' type='number' placeholder='R$'/>
+                <br>
+                <input class = 'new-input' type='text' placeholder='Descrição'/>
+                <br>
+                <select id='add-money-categorias' class='select-categorias'>
+                <option>Categoria</option>
+                <option value='0'>Salário</option>
+                <option value='1'>Troco</option>
+                <option value='2'>Econômias</option>
+                </select>
             </div>
             <div class="modal-footer">
-                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
-                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
-                        style="background-color:#a3130d;">Se Inscrever</button></a>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+               <button type="button" class="btn btn-success">Confirmar</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
             </div>
         </div>
     </div>
@@ -58,20 +71,18 @@ function MyPlanejament(){
     document.querySelector('.modal-add-action').innerHTML=`<div id="myModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-  
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        
+            <div class="modal-header" style='background-color:#0984e3;'>
+            <h1 style='font-size:20px;'>Planejamento</h1>
+                                
             </div>
             <div class="modal-body">
-                <img style="width: 100%;height: auto;" src="images/components/modal-image.jpeg" />
+                <label for='new-input'>Valor a Ser Planejado:</label>
+                <input class ='new-input' type='number' placeholder='R$'/>
             </div>
             <div class="modal-footer">
-                <a href="https://www.vestibulinhoetec.com.br/home/" target="_blank"
-                    rel="noopener noreferrer"><button type="button" class="btn btn-primary bg-faded"
-                        style="background-color:#a3130d;">Se Inscrever</button></a>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+               <button type="button" class="btn btn-success">Confirmar</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
             </div>
         </div>
     </div>
